27ec0b7a845e9698a89f42c3f6da55bf8d2ca7ee
codacdanny <70378536+codacdanny@users.noreply.github.com>
2022-05-19 22:39:13 +0100

    updated files

---
+++ shell.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * main - The main shell program
+ * @ac: The argument count
+ * @av: The argument vector
+ *
+ * Return: Always zero (0)
+ */
+
+int main(__attribute__((unused)) int ac, __attribute__((unused)) char **av)
+{
+	char *line = NULL;
+	size_t lineCap = 0;
+	size_t lineReturn = 1;
+	int stats;
+	int flag;
+	char **toks;
+	pid_t childPid;
+
+	while (lineReturn > 0)
+	{
+		flag = 1;
+		printf("$ ");
+		fflush(stdout);
+		lineReturn = getline(&line, &lineCap, stdin);
+		if (lineReturn == ULONG_MAX)
+			break;
+		toks = malloc(sizeof(char *));
+		toks[0] = trim(_strdup(line));
+		if (flag == 1)
+			childPid = fork();
+		if (childPid != -1 && flag == 1)
+		{
+			if (childPid == 0)
+			{
+				execve(toks[0], toks, NULL);
+				perror(av[0]);
+				exit(1);
+			}
+			else
+			{
+				wait(&stats);
+			}
+		}
+	}
+	return (0);
+}
